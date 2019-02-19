e6ba6a89cd04d0bce57a8177c58bf3def709d964
Semhar Sium <semhar50@gmail.com>
2019-02-19 22:50:40 +0100

    modifier image logo width imported

--- src/patterns/atoms/image/index.js
+++ src/patterns/atoms/image/index.js
@@ -1,3 +1,4 @@
 import './css/image.scss';
-import './css/modifier/image-fullwidth.scss';
+import './css/modifier/image-logo-width.scss';
+import './css/modifier/image-halfwidth.scss';
 import './js/image';
