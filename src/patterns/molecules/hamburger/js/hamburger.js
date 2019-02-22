cb2b35ed2b3099388fb2499b99a677a71d1695fa
Semhar Sium <semhar50@gmail.com>
2019-02-22 23:51:32 +0100

    hamburger js file created

---
+++ src/patterns/molecules/hamburger/js/hamburger.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import * as T from 'terrific';
+import $ from 'jquery';
+
+/**
+ * hamburger module implementation.
+ *
+ * @author Semhar Sium <semhar50@gmail.com>
+ */
+
+// const selectors = { button: '.js-hamburger__button' };
+// const stateClasses = { disabled: 'state-hamburger--disabled' };
+
+T.Module.Hamburger = T.createModule({
+	selectors: {
+		hamburgerMenuItem: ".js-hamburger-item"
+	},
+	stateClasses: {
+		hamburgerMenuItemActive: "m-hamburger--menu-item-active"
+	},
+	start(resolve) {
+		this.$ctx = $(this._ctx);
+		this.$hamburgerItems = this.$ctx.find(this.selectors.hamburgerMenuItem);
+		this.registerHandlers();
+		resolve();
+	},
+
+	registerHandlers() {
+		this.$ctx.on("click", this.handleMenuItem.bind(this));
+	},
+
+	handleMenuItem() {
+		this.$hamburgerItems.toggleClass(this.stateClasses.hamburgerMenuItemActive);
+	}
+});
