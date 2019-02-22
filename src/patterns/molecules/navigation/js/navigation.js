6e7eaf9828a12d52f613e60f09392820a8497d10
Semhar Sium <semhar50@gmail.com>
2019-02-22 23:58:45 +0100

    navigation js file created

--- src/patterns/molecules/navigation/js/navigation.js
+++ src/patterns/molecules/navigation/js/navigation.js
@@ -1,3 +1,4 @@
+import "../../hamburger/js/hamburger";
 'use strict';
 
 import * as T from 'terrific';
@@ -13,9 +14,25 @@ import $ from 'jquery';
 // const stateClasses = { disabled: 'state-navigation--disabled' };
 
 T.Module.Navigation = T.createModule({
+	selectors: {
+		navigationMenu: ".js-navigation-menu"
+	},
+	stateClasses: {
+		navigationMenuActive: "m-navigation--menu-active"
+	},
 	start(resolve) {
-		const $ctx = $(this._ctx);
-
+		this.$ctx = $(this._ctx);
+		this.$navigationMenu = this.$ctx.find(this.selectors.navigationMenu);
+		this.registerHandlers();
 		resolve();
 	},
+
+	registerHandlers() {
+		this.$ctx.on("click", this.handleMenuItem.bind(this));
+	},
+
+	handleMenuItem() {
+		this.$navigationMenu.toggleClass(this.stateClasses.navigationMenuActive);
+	}
 });
+
