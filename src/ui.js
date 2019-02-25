54abcfccf87c848d420e2abe9c2604b5e7f4adba
Semhar Sium <semhar50@gmail.com>
2019-02-25 21:50:19 +0100

    commited and color2 scss file imported

--- src/ui.js
+++ src/ui.js
@@ -5,12 +5,14 @@ import * as T from 'terrific';
 import $ from 'jquery';
 import { hot } from '@gondel/plugin-hot';
 
+// This because we need styles on shared
 import './shared/base/security/js/security';
 import './shared/base/reset/css/reset.scss';
 import './shared/utils/grid/css/grid.scss';
 import './shared/base/document/css/document.scss';
 import './shared/base/webfonts/css/webfont-gaegu.scss';
 import './shared/base/webfonts/css/webfont-playfair-display.scss';
+import './shared/utils/colors2/css/colors2.scss';
 
 // This because we need styles on views
 import './views/css/aboutme.scss';
@@ -19,9 +21,15 @@ import './views/css/index.scss';
 
 import './patterns/molecules/navigation';
 import './patterns/molecules/hamburger';
+import './patterns/molecules/paragraph';
+import './patterns/molecules/footer';
 import './patterns/atoms/heading';
 import './patterns/atoms/image';
 import './patterns/atoms/button';
+import './patterns/atoms/list-items';
+import './patterns/atoms/spacer';
+
+
 
 
 hot(module);
