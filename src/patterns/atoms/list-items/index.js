adf9f17e41ea8f447456b066bd8951f813d5fea0
Semhar Sium <semhar50@gmail.com>
2019-03-02 02:37:47 +0100

    modifier imported

--- src/patterns/atoms/list-items/index.js
+++ src/patterns/atoms/list-items/index.js
@@ -1,3 +1,6 @@
 import './css/list-items.scss';
 import './css/modifier/list-items-home.scss';
+import './css/modifier/list-items-aboutMe-professional-works.scss';
+import './css/modifier/list-items-aboutMe-further-education.scss';
+import './css/modifier/list-items-aboutMe-education.scss';
 import './js/list-items';
