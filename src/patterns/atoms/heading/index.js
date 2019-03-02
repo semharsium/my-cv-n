6c3f73f30b7e7436a7f5a7f4032dc8701f020a5b
Semhar Sium <semhar50@gmail.com>
2019-03-02 02:27:56 +0100

    modifier imported

--- src/patterns/atoms/heading/index.js
+++ src/patterns/atoms/heading/index.js
@@ -1,3 +1,7 @@
 import './css/heading.scss';
 import './css/modifier/heading-centered.scss';
+import './css/modifier/heading-centered-professional.scss';
+import './css/modifier/heading-centered-education.scss';
+import './css/modifier/heading-centered-furtherEducation.scss';
 import './js/heading';
+
