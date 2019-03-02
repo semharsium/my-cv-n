c5623480464b20d713a46baca8b48bbf45608bef
Semhar Sium <semhar50@gmail.com>
2019-03-02 02:32:19 +0100

    modifier imported

--- src/patterns/atoms/image/index.js
+++ src/patterns/atoms/image/index.js
@@ -1,2 +1,6 @@
 import './css/image.scss';
+import './css/modifier/image-sheep.scss';
+import './css/modifier/image-lisa.scss';
+import './css/modifier/image-road.scss';
+import './css/modifier/image-calculator.scss';
 import './js/image';
