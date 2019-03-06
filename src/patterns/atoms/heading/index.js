7ac1509e6ad68b7113544ea95a6f34982975487b
Semhar Sium <semhar50@gmail.com>
2019-03-06 15:57:40 +0100

    modifier heading centerd tasks and sub heading imported

--- src/patterns/atoms/heading/index.js
+++ src/patterns/atoms/heading/index.js
@@ -3,5 +3,7 @@ import './css/modifier/heading-centered.scss';
 import './css/modifier/heading-centered-professional.scss';
 import './css/modifier/heading-centered-education.scss';
 import './css/modifier/heading-centered-furtherEducation.scss';
+import './css/modifier/heading-centered-tasks.scss';
+import './css/modifier/heading-centered-sub-heading.scss';
 import './js/heading';
 
