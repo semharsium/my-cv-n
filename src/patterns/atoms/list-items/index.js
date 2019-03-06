b602fe7a7bdd76e6bd322b303200e235649d3449
Semhar Sium <semhar50@gmail.com>
2019-03-06 16:03:13 +0100

    modifier list items work project one and two

--- src/patterns/atoms/list-items/index.js
+++ src/patterns/atoms/list-items/index.js
@@ -3,4 +3,6 @@ import './css/modifier/list-items-home.scss';
 import './css/modifier/list-items-aboutMe-professional-works.scss';
 import './css/modifier/list-items-aboutMe-further-education.scss';
 import './css/modifier/list-items-aboutMe-education.scss';
+import './css/modifier/list-items-work-project-one.scss';
+import './css/modifier/list-items-work-project-two.scss';
 import './js/list-items';
