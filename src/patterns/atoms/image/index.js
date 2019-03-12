a5134171777248945641a36a6c691af94e280ba1
Semhar Sium <semhar50@gmail.com>
2019-03-12 11:24:51 +0100

    modifiers image slider scss and image slider two scss imported

--- src/patterns/atoms/image/index.js
+++ src/patterns/atoms/image/index.js
@@ -3,4 +3,6 @@ import './css/modifier/image-sheep.scss';
 import './css/modifier/image-lisa.scss';
 import './css/modifier/image-road.scss';
 import './css/modifier/image-calculator.scss';
+import './css/modifier/image-slider.scss';
+import './css/modifier/image-slider-two.scss';
 import './js/image';
