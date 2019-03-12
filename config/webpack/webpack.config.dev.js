2028f05d1bf1d7692f4d5c8e0172b182bbcfc304
Semhar Sium <semhar50@gmail.com>
2019-03-12 11:18:38 +0100

    eot merged to  webpack configuration

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -1,4 +1,15 @@
+const merge = require('webpack-merge');
 const options = require('./options');
 const webpackConfig = require('@nitro/webpack/webpack-config/webpack.config.dev')(options);
 
-module.exports = webpackConfig;
+module.exports = merge.smart(webpackConfig, {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(eot)$/,
+                loader: require.resolve('file-loader'),
+            },
+        ],
+    },
+});
